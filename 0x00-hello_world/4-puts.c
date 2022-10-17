0c54715115c107efe195c6a48157f7a6e60339f5
femybadejo <femybadejo@gmail.com>
2022-10-17 15:10:43 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * return : Always 0
+ * main - Entry point
+ * return : Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
