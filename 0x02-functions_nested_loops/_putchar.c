17320ac53031375b5620055ba6361783625710e4
femybadejo <femybadejo@gmail.com>
2022-10-28 13:59:03 -0700

    lets go

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,32 +2,12 @@
 #include <unistd.h>
 
 /**
- *
  * _putchar -write the charater c to stdout
  * @c: the character to print
- *
- * Return: on success 1
+ * Return: On success 1
  */
+
 int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+{
+	return (write(1, &c, 1));
+};
