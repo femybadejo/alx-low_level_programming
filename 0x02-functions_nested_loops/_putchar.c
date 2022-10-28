c8c623929b43407b9821215ffd157be97b27d8c5
femybadejo <femybadejo@gmail.com>
2022-10-28 05:15:42 -0700

    lets go

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *
+ * _putchar -write the charater c to stdout
+ * @c: the character to print
+ *
+ * Return: on success 1
+ */
+int _putchar(char c)
+	{
+		return (write(1, &c, 1));
+	}
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
